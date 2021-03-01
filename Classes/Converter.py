25ed1f4621dc3083eb725e29a4e33f47f37eb3e8
Jacob Janak <jakeathotmail@gmail.com>
2021-03-01 10:50:00 -0500

    Abstracted away data conversion into its own class

---
+++ Classes/Converter.py
@@ -0,0 +1,65 @@
+"""
+This class converts data from the format that is given by the nueral network, which is
+a parallel array, into an array of objects. For example:
+
+This:
+    { "img_001": {"labels": ["tooth_1", "tooth_2"], "scores": [0.5, 0.2]} }
+
+Becomes:
+    [ {id: "img_001", inputBoxes: [{label: "tooth_1", score: 0.5}, {label: "tooth_2", score: 0.2}] } ]
+"""
+
+from .Image import Image
+from .InputBox import InputBox
+
+
+class Converter:
+
+    def __init__(self, unconverted):
+        self.result = self.parse_image_list(unconverted)
+        
+
+    def parse_image_list(self, images_dict):
+        """
+        Converts data from the CSV format into a list of Image objects.
+
+        Args:
+            images_dict: data formatted as dicts of parallel arrays
+            i.e. { "img_001": {"labels": ["tooth_1", "tooth_2"], "scores": [0.5, 0.2]} }
+
+        Returns:
+            arr: array of Image objects
+        """
+
+        images = []
+        for key, value in images_dict.items():
+            boxes = self.parse_box_list(value)
+            images.append(Image(key, boxes))
+
+        return images
+
+
+    def parse_box_list(self, image_dict):
+        """
+        Converts data from the CSV format into a list of Box objects for an Image.
+
+        Args:
+            image_dict: data formatted as a dict of parallel arrays
+            i.e. {"labels": ["tooth_1", "tooth_2"], "scores": [0.5, 0.2]}
+
+        Returns:
+            arr: array of InputBox objects
+        """
+
+        boxes = []
+        for i in range(len(image_dict["labels"])):
+            boxes.append(InputBox(
+                image_dict["labels"][i],
+                image_dict["x1s"][i],
+                image_dict["y1s"][i],
+                image_dict["x2s"][i],
+                image_dict["y2s"][i],
+                image_dict["scores"][i]
+            ))
+
+        return boxes
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 
 # Classes
 from Classes.CSVReader import CSVReader
+from Classes.Converter import Converter
 from Classes.Image import Image
 from Classes.InputBox import InputBox
+
 
 # File paths
 img_folder = "/Users/jacobjanak/Documents/Code/VideaHealth/CS410_VideaHealth_sample_data/images"
@@ -10,57 +12,16 @@ file_gt = "/Users/jacobjanak/Documents/Code/VideaHealth/CS410_VideaHealth_sample
 file_pred = "/Users/jacobjanak/Documents/Code/VideaHealth/CS410_VideaHealth_sample_data/2_input_model_predictions_2.csv"
 
 
-def parse_image_list(images_dict):
-    """
-    Converts data from the CSV format into a list of Image objects.
-
-    Args:
-        images_dict: data formatted as dicts of parallel arrays
-        i.e. { "img_001": {"labels": ["tooth_1", "tooth_2"], "scores": [0.5, 0.2]} }
-
-    Returns:
-        arr: array of Image objects
-    """
-
-    images = []
-    for key, value in images_dict.items():
-        boxes = parse_box_list(value)
-        images.append(Image(key, boxes))
-
-    return images
-
-
-def parse_box_list(image_dict):
-    """
-    Converts data from the CSV format into a list of Box objects for an Image.
-
-    Args:
-        image_dict: data formatted as a dict of parallel arrays
-        i.e. {"labels": ["tooth_1", "tooth_2"], "scores": [0.5, 0.2]}
-
-    Returns:
-        arr: array of InputBox objects
-    """
-
-    boxes = []
-    for i in range(len(image_dict["labels"])):
-        boxes.append(InputBox(
-            image_dict["labels"][i],
-            image_dict["x1s"][i],
-            image_dict["y1s"][i],
-            image_dict["x2s"][i],
-            image_dict["y2s"][i],
-            image_dict["scores"][i]
-        ))
-
-    return boxes
-
-
-
+# Read the input CSV file
 Reader = CSVReader(file_pred)
-images = parse_image_list(Reader.output)
 
+# Convert the data in the CSV into a more usable format
+DataConverter = Converter(Reader.output)
 
+# Retrieve the image list
+images = DataConverter.result
+
+# Logging
 print(images)
 print(images[0])
 print(images[0].id)
