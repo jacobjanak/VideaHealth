81c5f1df3e9bb5c64e87a469114cb3680ce2b55d
Jacob Janak <jakeathotmail@gmail.com>
2021-03-10 15:48:35 -0500

    Created outputbox class

---
+++ Classes/OutputBox.py
@@ -0,0 +1,13 @@
+"""
+TO DO: Description
+"""
+
+
+class InputBox:
+
+    def __init__(self, label, x1s, y1s, x2s, y2s):
+        self.label = label
+        self.x1s = x1s
+        self.y1s = y1s
+        self.x2s = x2s
+        self.y2s = y2s
