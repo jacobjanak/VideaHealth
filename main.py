b79385c018518272b75c595bfa1e45b42f777438
Danley Nemorin <danleynemo1995@gmail.com>
2021-03-26 20:57:59 -0400

    Added argparse to add parameters to the functions

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 import os
 
@@ -12,11 +13,27 @@ from Tests.accuracy import accuracy
 from Tests.visualizer import visualizer
 
 # File paths
+
+
+my_parser = argparse.ArgumentParser()
+
+my_parser.add_argument('-gt', '-groundtruthfile', help='CSV File of GroundTruth to compare with')
+my_parser.add_argument('-pf', '-predfile', help='CSV File of the Neural Network Prediction Data')
+my_parser.add_argument('-i', '-imgdirectory', help='Directory where the Images related to the Dataset is located')
+
+args = my_parser.parse_args()
+
+'''
+#Older Method
 project_dir = os.path.dirname(os.path.abspath(__file__))
 data_dir = project_dir + "/CS410_VideaHealth_sample_data"
 img_folder = data_dir + "/images"
 file_gt = data_dir + "/1_ground_truth_2a.csv"
 file_pred = data_dir + "/2_input_model_predictions_2.csv"
+'''
+img_folder = args.i
+file_gt = args.gt
+file_pred = args.pf
 
 # Read the input CSV file
 input_raw = CSVReader(file_pred).output
@@ -27,22 +44,24 @@ gt_raw = CSVReader(file_gt).output
 images_gt = Converter(gt_raw).result
 
 ############ Test post processing scripts
-#print("\nTesting haehn script:")
-#from Scripts.haehn import haehn
-#images_pred = haehn(images_input)
-#accuracy(images_pred, images_gt)
+print("\nTesting haehn script:")
+from Scripts.haehn import haehn
+images_pred = haehn(images_input)
+accuracy(images_pred, images_gt)
 #visualizer(images_pred, images_gt)
 
 print("\nTesting best_box script:")
 from Scripts.best_box import best_box
 images_pred = best_box(images_input)
 accuracy(images_pred, images_gt)
-visualizer(images_pred, images_gt)
+#visualizer(images_pred, images_gt)
 
 print("\nTesting nms script:")
 from Scripts.non_maximum_suppression import nonmaximum_suppression
 images_pred = nonmaximum_suppression(images_input)
 accuracy(images_pred, images_gt)
-visualizer(images_pred, images_gt)
+#visualizer(images_pred, images_gt)
 
-print()
+
+
+print("PredictedBoxes saved as PrediectedBoxes.csv")
