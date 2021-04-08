4fad150f8b663d99d549f3bf7dd27e2994f9e2e2
Jacob Janak <jakeathotmail@gmail.com>
2021-04-08 13:34:36 -0600

    Oops I changed main.py in my last commit so Im undoing that

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from Classes.Box import Box
 # Import accuracy script for testing
 from Tests.accuracy import accuracy
 from Tests.accuracy2 import accuracy2
-# from Tests.visualizer import visualizer
+from Tests.visualizer import visualizer
 
 # Import teeth arrangement script to correct teeth classification
 from Scripts.teeth_arrangement import teeth_arrangements
@@ -18,10 +18,10 @@ from Scripts.relabel import relabel
 
 # File paths
 project_dir = os.path.dirname(os.path.abspath(__file__))
-data_dir = project_dir + "/CS410_VideaHealth_full_data"
+data_dir = project_dir + "/CS410_VideaHealth_sample_data"
 img_folder = data_dir + "/images"
-file_gt = data_dir + "/1_ground_truth.csv"
-file_pred = data_dir + "/2_input_model_predictions.csv"
+file_gt = data_dir + "/1_ground_truth_2a.csv"
+file_pred = data_dir + "/2_input_model_predictions_2.csv"
 
 # Read the input CSV file
 input_raw = CSVReader(file_pred).output
@@ -45,25 +45,25 @@ print("\nTesting best_box script:")
 from Scripts.best_box import best_box
 images_pred = best_box(images_input)
 # teeth_arrangements(images_pred)
-# relabel(images_pred)
+relabel(images_pred)
 accuracy(images_pred, images_gt)
 accuracy2(images_pred, images_gt)
 # visualizer('best_box', images_pred, images_gt)
 
-# print("\nTesting nms script:")
-# from Scripts.non_maximum_suppression import nonmaximum_suppression
-# images_pred = nonmaximum_suppression(images_input)
-# # teeth_arrangements(images_pred)
-# relabel(images_pred)
-# accuracy(images_pred, images_gt)
-# accuracy2(images_pred, images_gt)
-# # visualizer('nms', images_pred, images_gt)
+print("\nTesting nms script:")
+from Scripts.non_maximum_suppression import nonmaximum_suppression
+images_pred = nonmaximum_suppression(images_input)
+# teeth_arrangements(images_pred)
+relabel(images_pred)
+accuracy(images_pred, images_gt)
+accuracy2(images_pred, images_gt)
+# visualizer('nms', images_pred, images_gt)
 
 print("\nTesting best cluster haehn script:")
 from Scripts.best_cluster_haehn import best_cluster_haehn
 images_pred = best_cluster_haehn(images_input)
 # teeth_arrangements(images_pred)
-# relabel(images_pred)
+relabel(images_pred)
 accuracy(images_pred, images_gt)
 accuracy2(images_pred, images_gt)
 # visualizer('nms', images_pred, images_gt)
