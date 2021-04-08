6bf7902fdfdab97c7e33f663da8f02f6028bc5a7
Jacob Janak <jakeathotmail@gmail.com>
2021-04-08 12:35:06 -0600

    Added in IOU accuracy script

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from Classes.Box import Box
 
 # Import accuracy script for testing
 from Tests.accuracy import accuracy
+from Tests.accuracy2 import accuracy2
 from Tests.visualizer import visualizer
 
 # Import teeth arrangement script to correct teeth classification
@@ -37,6 +38,7 @@ images_pred = haehn(images_input)
 # teeth_arrangements(images_pred)
 relabel(images_pred)
 accuracy(images_pred, images_gt)
+accuracy2(images_pred, images_gt)
 # visualizer('haehn', images_pred, images_gt)
 
 print("\nTesting best_box script:")
@@ -45,6 +47,7 @@ images_pred = best_box(images_input)
 # teeth_arrangements(images_pred)
 relabel(images_pred)
 accuracy(images_pred, images_gt)
+accuracy2(images_pred, images_gt)
 # visualizer('best_box', images_pred, images_gt)
 
 print("\nTesting nms script:")
@@ -53,6 +56,7 @@ images_pred = nonmaximum_suppression(images_input)
 # teeth_arrangements(images_pred)
 relabel(images_pred)
 accuracy(images_pred, images_gt)
+accuracy2(images_pred, images_gt)
 # visualizer('nms', images_pred, images_gt)
 
 print("\nTesting best cluster haehn script:")
@@ -61,6 +65,7 @@ images_pred = best_cluster_haehn(images_input)
 # teeth_arrangements(images_pred)
 relabel(images_pred)
 accuracy(images_pred, images_gt)
+accuracy2(images_pred, images_gt)
 # visualizer('nms', images_pred, images_gt)
 
 print()
